f76896738ed28b680299423ae973e6e6673269df
david_smith <david_smith@sweetwater.com>
2025-01-04 15:01:45 -0500

    maint/optimize-testing Updated `test.sh`.

--- test.sh
+++ test.sh
@@ -1,16 +1,12 @@
-# run-tests.sh
+#!/bin/bash
 set -e
 
 php_versions=("php83" "php82" "php81" "php80" "php74" "php73" "php72" "php71")
 
 for version in "${php_versions[@]}"; do
-  echo "Setting up environment..."
-  rm composer.lock
-  docker compose run --rm "$version"composer composer install
-  echo "Running tests on $version..."
+  docker compose run --rm "${version}composer" composer update --no-cache
+
   if ! docker compose run --rm "$version" vendor/bin/phpunit; then
-    echo "Tests failed on $version."
     exit 1
   fi
 done
-echo "All tests passed!"
