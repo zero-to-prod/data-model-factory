b8b89a27e484967b2700fdd2bc162a36affa29bc
David Smith <dasmith@zero-to-prod.com>
2025-02-15 09:37:23 -0500

    refactor/factory Updated `src/Factory.php`.

--- src/Factory.php
+++ src/Factory.php
@@ -48,84 +48,7 @@ namespace Zerotoprod\DataModelFactory;
  */
 trait Factory
 {
-    /**
-     * Stores the values for the class.
-     *
-     * @var array
-     *
-     * @link https://github.com/zero-to-prod/data-model-factory
-     *
-     * @see  https://github.com/zero-to-prod/data-model
-     */
-    private $context;
-
-    /**
-     * Values to instantiate the class.
-     *
-     * @link https://github.com/zero-to-prod/data-model-factory
-     *
-     * @see  https://github.com/zero-to-prod/data-model
-     */
-    public function __construct(array $context = [])
-    {
-        $this->context = array_merge($this->definition(), $context);
-    }
-
-    /**
-     * Define the class's default values.
-     *
-     * @return array<string, mixed>
-     *
-     * @link https://github.com/zero-to-prod/data-model-factory
-     *
-     * @see  https://github.com/zero-to-prod/data-model
-     */
-    private function definition(): array
-    {
-        return [];
-    }
-
-    /**
-     * Merge the context with the new values.
-     *
-     * @link https://github.com/zero-to-prod/data-model-factory
-     *
-     * @see  https://github.com/zero-to-prod/data-model
-     */
-    public function merge(array $definition = []): self
-    {
-        $this->context = array_merge($this->context, $definition);
-
-        return $this;
-    }
-
-    /**
-     * Merge the context with the new values and return the new context.
-     *
-     * @return array<string, mixed>
-     *
-     * @link https://github.com/zero-to-prod/data-model-factory
-     *
-     * @see  https://github.com/zero-to-prod/data-model
-     */
-    public function context(): array
-    {
-        return $this->context;
-    }
-
-    /**
-     * Get a new factory instance for the model
-     *
-     * @return self
-     *
-     * @link https://github.com/zero-to-prod/data-model-factory
-     *
-     * @see  https://github.com/zero-to-prod/data-model
-     */
-    public static function factory(array $context = []): self
-    {
-        return new static($context);
-    }
+    use \Zerotoprod\Factory\Factory;
 
     /**
      * Instantiates the class using `$this->context`.
@@ -136,7 +59,7 @@ trait Factory
      */
     private function instantiate()
     {
-        return $this->model::from(array_merge($this->definition(), $this->context));
+        return $this->model::from($this->resolve());
     }
 
     /**
@@ -156,97 +79,5 @@ trait Factory
     public function make()
     {
         return $this->instantiate();
-    }
-
-    /**
-     * Mutate the context before instantiating the class.
-     *
-     * ```
-     *   public function setValue($value): self
-     *   {
-     *        return $this->state('value.nested', $value);
-     *   }
-     *
-     *  public function setValue($value): self
-     *  {
-     *       return $this->state(['value' => $value]);
-     *  }
-     *
-     *  public function setValueWithClosure($value): self
-     *  {
-     *      return $this->state(function ($context) use ($value) {
-     *          return ['value' => $value];
-     *      });
-     *  }
-     * ```
-     *
-     * @param  callable|array  $state
-     *
-     * @return self
-     *
-     * @link https://github.com/zero-to-prod/data-model-factory
-     *
-     * @see  https://github.com/zero-to-prod/data-model
-     */
-    private function state($state, $value = null): self
-    {
-        if (is_string($state)) {
-            $ref = &$this->context;
-
-            foreach (explode('.', $state) as $key) {
-                if (!isset($ref[$key]) || !is_array($ref[$key])) {
-                    $ref[$key] = [];
-                }
-                $ref = &$ref[$key];
-            }
-
-            $ref = $value;
-
-            return $this;
-        }
-
-        $this->context = array_merge(
-            $this->context,
-            is_callable($state) ? $state($this->context) : $state
-        );
-
-        return $this;
-    }
-
-    /**
-     * Mutate the context before instantiating the class.
-     *
-     * ```
-     *   public function setValue($value): self
-     *   {
-     *        return $this->state('value.nested', $value);
-     *   }
-     *
-     *  public function setValue($value): self
-     *  {
-     *       return $this->state(['value' => $value]);
-     *  }
-     *
-     *  public function setValueWithClosure($value): self
-     *  {
-     *      return $this->state(function ($context) use ($value) {
-     *          return ['value' => $value];
-     *      });
-     *  }
-     * ```
-     *
-     * @param  callable|array  $state
-     *
-     * @return self
-     *
-     * @link https://github.com/zero-to-prod/data-model-factory
-     *
-     * @see  https://github.com/zero-to-prod/data-model
-     */
-    public function set($state, $value = null): self
-    {
-        $this->state($state, $value);
-
-        return $this;
     }
 }
